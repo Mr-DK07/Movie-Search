3f127d1b080d650dc80823f767b36e058bf6d90b
D K <143409270+Mr-DK07@users.noreply.github.com>
2025-02-07 02:04:20 +0530

    Update movie.js

--- movie.js
+++ movie.js
@@ -5,7 +5,7 @@ const inputBox = document.querySelector(".inputBox");
 // Function to fetch movie details using OMDB Api
 const getMovieInfo = async (movie) => {
   try {
-    const url = `http://www.omdbapi.com/?&apikey=myAPIKey&t=${movie}`;
+    const url = `https://www.omdbapi.com/?&apikey=myAPIKey&t=${movie}`;
 
     const response = await fetch(url);
 
